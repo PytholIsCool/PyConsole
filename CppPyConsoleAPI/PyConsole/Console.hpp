fb2310b87e0661fe869abe5a0849183c438b563f
Pythol <95198306+PytholIsCool@users.noreply.github.com>
2025-06-19 22:06:55 -0400

    Update Console.hpp
    General improvements + added functionality with templates

--- CppPyConsoleAPI/PyConsole/Console.hpp
+++ CppPyConsoleAPI/PyConsole/Console.hpp
@@ -1,10 +1,15 @@
 #ifndef CONSOLE_HPP
 #define CONSOLE_HPP
 
+#define NOMINMAX
 #include <windows.h>
 #include <iostream>
 #include <list>
+#include <sstream>
 #include <string>
+#include <regex>
+#include <iomanip>
+#include <type_traits>
 
 class Console {
 public:
@@ -60,98 +65,164 @@ private:
     static void WriteRGBText(COLORREF color, const std::string& text);
 
     // Templates And Formatting
-    static void ReplaceFirst(std::string& str, const std::string& token, int value) {
-        str.replace(str.find(token), token.length(), std::to_string(value));
-    }
+    static void FixAnonymousPlaceholders(std::string& str);
 
-    static void ReplaceFirst(std::string& str, const std::string& token, float value) {
-        str.replace(str.find(token), token.length(), std::to_string(value));
-    }
+    static void ReplaceFirst(std::string& str, const std::string& token, int value);
 
-    static void ReplaceFirst(std::string& str, const std::string& token, double value) {
-        str.replace(str.find(token), token.length(), std::to_string(value));
-    }
+    static void ReplaceFirst(std::string& str, const std::string& token, float value);
 
-    static void ReplaceFirst(std::string& str, const std::string& token, bool value) {
-        str.replace(str.find(token), token.length(), value ? "true" : "false");
-    }
+    static void ReplaceFirst(std::string& str, const std::string& token, double value);
 
-    static void ReplaceFirst(std::string& str, const std::string& token, const std::string& value) {
-        str.replace(str.find(token), token.length(), value);
-    }
+    static void ReplaceFirst(std::string& str, const std::string& token, bool value);
 
-    static void ReplaceFirst(std::string& str, const std::string& token, const char* value) {
-        str.replace(str.find(token), token.length(), value ? value : "(null)");
-    }
+    static void ReplaceFirst(std::string& str, const std::string& token, const std::string& value);
+
+    static void ReplaceFirst(std::string& str, const std::string& token, const char* value);
+
+    static void ReplaceFirst(std::string& str, const std::string& token, ...);
+
+    static void ReplaceFormatted(std::string& str, const std::string& token, const std::string& fmt, uint64_t value);
 
     template<typename T>
     static auto ReplaceFirst(std::string& str, const std::string& token, const T& value)
         -> decltype(std::declval<std::ostringstream&>() << value, void()) {
         std::ostringstream oss;
         oss << value;
-        str.replace(str.find(token), token.length(), oss.str());
+
+        size_t pos = str.find(token);
+        if (pos != std::string::npos && pos + token.length() <= str.length()) {
+            str.replace(pos, token.length(), oss.str());
+            return;
+        }
+
+        // Add this log to track why nothing happened
+        Console::Error(">>> Nothing replaced for token '{}'", token);
     }
 
-    static void ReplaceFirst(std::string& str, const std::string& token, ...) {
-        str.replace(str.find(token), token.length(), "[[unprintable]]");
+    template<typename T>
+    static void ReplaceToken(std::string& str, int index, const T& value) {
+        std::ostringstream finalOutput;
+        std::regex pattern(R"(\{)" + std::to_string(index) + R"((?::([^}]+))?\})");
+
+        std::sregex_iterator it(str.begin(), str.end(), pattern);
+        std::sregex_iterator end;
+
+        size_t lastEnd = 0;
+        for (; it != end; ++it) {
+            finalOutput << str.substr(lastEnd, it->position() - lastEnd);
+
+            std::ostringstream oss;
+            std::string fmt = (*it)[1];
+            char fillChar = ' ';
+            int width = 0;
+            bool upper = false;
+            bool hex = false;
+
+            if (!fmt.empty()) {
+                if (fmt.find('X') != std::string::npos) { hex = true; upper = true; }
+                else if (fmt.find('x') != std::string::npos) hex = true;
+
+                size_t digitStart = fmt.find_first_of("0123456789");
+                if (digitStart != std::string::npos) {
+                    width = std::stoi(fmt.substr(digitStart));
+                    if (fmt[digitStart] == '0') fillChar = '0';
+                }
+            }
+
+            if constexpr (std::is_same_v<T, bool>) {
+                oss << std::boolalpha << value;
+            }
+            else if constexpr (std::is_integral_v<T>) {
+                if (hex) {
+                    oss << std::setfill(fillChar) << std::setw(width)
+                        << (upper ? std::uppercase : std::nouppercase)
+                        << std::hex << value;
+                }
+                else {
+                    oss << std::setfill(fillChar) << std::setw(width) << value;
+                }
+            }
+            else {
+                oss << value;
+            }
+
+            finalOutput << oss.str();
+            lastEnd = it->position() + it->length();
+        }
+
+        finalOutput << std::string(str.begin() + lastEnd, str.end());
+        str = finalOutput.str();
+
+        // Also replace raw {N}
+        std::string simpleToken = "{" + std::to_string(index) + "}";
+        std::ostringstream raw;
+        if constexpr (std::is_same_v<T, bool>)
+            raw << std::boolalpha << value;
+        else
+            raw << value;
+
+        size_t pos = 0;
+        while ((pos = str.find(simpleToken, pos)) != std::string::npos) {
+            str.replace(pos, simpleToken.length(), raw.str());
+            pos += raw.str().length();
+        }
     }
 
     template<typename T, typename... Rest>
     static void ReplaceFirstAt(std::string& str, int index, const T& value, const Rest&... rest) {
-        ReplaceFirst(str, "{" + std::to_string(index) + "}", value);
-        if constexpr (sizeof...(rest) > 0) {
+        ReplaceToken(str, index, value);
+        if constexpr (sizeof...(rest) > 0)
             ReplaceFirstAt(str, index + 1, rest...);
-        }
     }
 
     template<typename... Args>
     static void FormatReplace(std::string& str, const Args&... args) {
+        FixAnonymousPlaceholders(str);
         ReplaceFirstAt(str, 0, args...);
     }
 
-    public:
-        template<typename... Args>
-        static void WriteLine(const std::string& formatStr, Args&&... args) {
-            std::string formatted = formatStr;
-            FormatReplace(formatted, std::forward<Args>(args)...);
-            WriteLine(formatted);
-        }
+public:
+    template<typename... Args>
+    static void WriteLine(const std::string& formatStr, Args&&... args) {
+        std::string formatted = formatStr;
+        FormatReplace(formatted, std::forward<Args>(args)...);
+        WriteLine(formatted);
+    }
 
-        template<typename... Args>
-        static void WriteLine(COLORREF color, const std::string& formatStr, Args&&... args) {
-            std::string formatted = formatStr;
-            FormatReplace(formatted, std::forward<Args>(args)...);
-            WriteLine(formatted, color);
-        }
+    template<typename... Args>
+    static void WriteLine(COLORREF color, const std::string& formatStr, Args&&... args) {
+        std::string formatted = formatStr;
+        FormatReplace(formatted, std::forward<Args>(args)...);
+        WriteLine(formatted, color);
+    }
 
-        template<typename... Args>
-        static void Write(const std::string& formatStr, Args&&... args) {
-            std::string formatted = formatStr;
-            FormatReplace(formatted, std::forward<Args>(args)...);
-            Write(formatted);
-        }
+    template<typename... Args>
+    static void Write(const std::string& formatStr, Args&&... args) {
+        std::string formatted = formatStr;
+        FormatReplace(formatted, std::forward<Args>(args)...);
+        Write(formatted);
+    }
 
-        template<typename... Args>
-        static void Write(COLORREF color, const std::string& formatStr, Args&&... args) {
-            std::string formatted = formatStr;
-            FormatReplace(formatted, std::forward<Args>(args)...);
-            Write(formatted, color);
-        }
+    template<typename... Args>
+    static void Write(COLORREF color, const std::string& formatStr, Args&&... args) {
+        std::string formatted = formatStr;
+        FormatReplace(formatted, std::forward<Args>(args)...);
+        Write(formatted, color);
+    }
 
-        template<typename... Args>
-        static void Error(const std::string& formatStr, Args&&... args) {
-            std::string formatted = formatStr;
-            FormatReplace(formatted, std::forward<Args>(args)...);
-            Error(formatted);
-        }
+    template<typename... Args>
+    static void Error(const std::string& formatStr, Args&&... args) {
+        std::string formatted = formatStr;
+        FormatReplace(formatted, std::forward<Args>(args)...);
+        Error(formatted);
+    }
 
-        template<typename... Args>
-        static void Debug(const std::string& formatStr, Args&&... args) {
-            if (isDebugMode == false)
-                return;
-            std::string formatted = formatStr;
-            FormatReplace(formatted, std::forward<Args>(args)...);
-            Debug(formatted);
-        }
+    template<typename... Args>
+    static void Debug(const std::string& formatStr, Args&&... args) {
+        if (!isDebugMode) return;
+        std::string formatted = formatStr;
+        FormatReplace(formatted, std::forward<Args>(args)...);
+        Debug(formatted);
+    }
 };
 #endif
